0de30da699ccd6c44efb12bc6394a1bd1814e90a
Vitalii <vitalii.hryshko.rss@gmail.com>
2023-07-31 11:59:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    const numbertoString = String(Math.abs(n));
+    let str = '';
+
+    /* Option 1 */
+    // for (let i = numbertoString.length - 1; i >= 0 ; i--) {
+    //     str = `${str}${numbertoString[i]}`;
+    // }
+
+    /* Option 2 */
+    str = numbertoString.split('').reverse().join('');
+
+    return str;
 }
